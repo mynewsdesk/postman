8eba7683eef098463a6dab81c16938fbd054b4db
Jonas Forsberg <himynameisjonas@gmail.com>
2014-02-03 16:31:29 +0100

    sinatra + ember ftw

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ ruby '2.1.0'
 
 gem 'sinatra'
 gem 'sinatra-assetpack', :require => 'sinatra/assetpack'
+gem 'sinatra-ember', :require => 'sinatra/ember'
 gem 'sass'
 gem 'coffee-script'
 
--- lib/app.rb
+++ lib/app.rb
@@ -4,23 +4,25 @@ class App < Sinatra::Base
   set :views, settings.root + 'app/views'
 
   register Sinatra::AssetPack
+  register Sinatra::Ember
 
+  ember {
+    templates '/js/templates.js', ['app/templates/**/*.hbs'], :relative_to => 'app/templates'
+  }
   assets {
-    serve '/js',     from: 'app/scripts'        # Default
-    serve '/css',    from: 'app/styles'       # Default
-    serve '/images', from: 'app/images'    # Default
+    serve '/js',     from: 'app/scripts'         # Default
+    serve '/css',    from: 'app/styles'          # Default
+    serve '/images', from: 'app/images'          # Default
+    serve '/bower', from: 'app/bower_components' # Default
 
     js :application, '/js/application.js', [
+      '/js/app.js',
       '/js/models/**/*.js',
       '/js/controllers/**/*.js',
       '/js/store/**/*.js',
       '/js/routes/**/*.js',
       '/js/views/**/*.js',
-      '/js/router.js'
-    ]
-
-    css :application, '/css/application.css', [
-      '/css/screen.css'
+      '/js/router.js',
     ]
 
     js_compression  :jsmin    # :jsmin | :yui | :closure | :uglify
